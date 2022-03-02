13376f9e02ee18041d27a8b19aae2d63b107d7b2
saloni-kelkar <100774909+saloni-kelkar@users.noreply.github.com>
2022-03-02 15:50:49 +0100

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -319,7 +319,6 @@ def update_graph(water_map_input,
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-#app.run_server(debug=True)
-    app.run_server(host='0.0.0.0',debug=True)
+    app.run_server(debug=True)
     
 #viewer.show(app)
